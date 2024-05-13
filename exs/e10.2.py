269b86bcea329ecc8a97f52600d531f139b41698
Mesorah <gabrielcambara123@gmail.com>
2024-05-13 18:56:20 -0300

    att

--- exs/e10.2.py
+++ exs/e10.2.py
@@ -1,7 +1,7 @@
 class Televisao:
-    def __init__(self, min, max):
+    def __init__(self, canal, min, max):
         self.ligada = False
-        self.canal = 2
+        self.canal = canal
         self.cmin = min
         self.cmax = max
 
@@ -13,7 +13,7 @@ class Televisao:
         if self.canal + 1 <= self.cmax:
             self.canal += 1
 
-tv = Televisao(1, 99)
+tv = Televisao(4, 1, 99)
 for x in range(0, 120):
     tv.muda_canal_para_cima()
 
--- exs/e10.3.py
+++ exs/e10.3.py
@@ -1,20 +1,24 @@
 class Televisao:
-    def __init__(self, min, max):
+    def __init__(self, canal, min, max):
         self.ligada = False
-        self.canal = 2
+        self.canal = canal
         self.cmin = min
         self.cmax = max
 
     def muda_canal_para_baixo(self):
         if self.canal - 1 >= self.cmin:
             self.canal -= 1
+        else:
+            self.canal = self.cmax
 
     def muda_canal_para_cima(self):
         if self.canal + 1 <= self.cmax:
             self.canal += 1
+        else:
+            self.canal = self.cmin
 
-tv = Televisao(1, 99)
-for x in range(0, 120):
-    tv.muda_canal_para_cima()
+tv = Televisao(9, 2, 10)
+tv.muda_canal_para_cima()
+tv.muda_canal_para_cima()
 
 print(tv.canal)
