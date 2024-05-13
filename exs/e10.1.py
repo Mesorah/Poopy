9e90e5983f7c5801764e83fa6ff5925b808b875a
Mesorah <gabrielcambara123@gmail.com>
2024-05-13 18:13:26 -0300

    att

---
+++ exs/e10.1.py
@@ -0,0 +1,10 @@
+class Televisao:
+    def __init__(self):
+        self.ligada = True
+        self.canal = 2
+        self.tamanho = 4
+        self.marca = 'Nike'
+
+
+tv = Televisao()
+print(tv.ligada)
